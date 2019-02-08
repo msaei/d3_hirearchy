c1d4d1941fda7d9c93c5d2f4bbf2c9f4d563477e
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-02-08 16:31:02 -0500

    data packec to array of circle object using pack

--- index.js
+++ index.js
@@ -22,8 +22,16 @@ const data = [
     { name: 'classical', parent: 'music', amount: 5 },
 ];
 
+// create canvas
+const svg = d3.select(".canvas")
+    .append('svg')
+    .attr('width', 1060)
+    .attr('height', 800)
+
+// create graph
+const graph = svg.append('g')
+    .attr('transform', 'translate(50,50)') // to give a 50 px margin
+
 const stratify = d3.stratify()
     .id(d => d.name)
     .parentId(d => d.parent);
-
-console.log(stratify(data));
